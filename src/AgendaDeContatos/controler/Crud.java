79e936bcb79c9b812f482d8e2440c739e9f8eb62
jose-rf <joserfranca13@gmail.com>
2025-04-30 22:26:48 -0300

    Criação do Projeto

---
+++ src/AgendaDeContatos/controler/Crud.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package AgendaDeContatos.controler;
+import java.sql.Connection;
+
+/**
+ *
+ * @author joser
+ */
+
+//Método CRUD
+public interface Crud<T> {
+    public boolean inserir(Connection connection, T entity);
+    public boolean atualizar(Connection connection, T entity);
+    public boolean deletar(Connection connection, T entity);
+    public T selecionar(String operador);
+}
